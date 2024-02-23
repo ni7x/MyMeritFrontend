9d700917270a184042661c750f681b53f6357571
ni7x <99970419+ni7x@users.noreply.github.com>
2024-02-23 13:18:30 +0100

    css fix

--- src/components/task_details/TaskSolutionWorkspace/components/FileTabManager/components/AddFileButton.tsx
+++ src/components/task_details/TaskSolutionWorkspace/components/FileTabManager/components/AddFileButton.tsx
@@ -56,16 +56,16 @@ const AddFileButton: React.FC<{}> = ({addFile, getFileByName}) => {
                 <div className="flex flex-col justify-center items-center bg-main-bg-color p-5 rounded">
                     {error != "" && <p className="pb-4 text-sm text-orange-400">{error}</p>}
                     <div className="mb-3 bg-secondary-bg-color rounded p-3">
-                        <input className=" bg-secondary-bg-color outline-0 rounded text-sm px-1" placeholder="enter file name" type={"text"} name={"name"} value={name} onChange={(e) => setName(e.target.value)} onKeyDown={handleKeyDown}/>
-                        <button className=" bg-secondary-bg-color rounded" onClick={createFile}><FontAwesomeIcon icon={faPlus}/></button>
+                        <input className="bg-secondary-bg-color outline-0 rounded text-sm px-1" placeholder="enter file name" type={"text"} name={"name"} value={name} onChange={(e) => setName(e.target.value)} onKeyDown={handleKeyDown}/>
+                        <button className="bg-secondary-bg-color rounded" onClick={createFile}><FontAwesomeIcon icon={faPlus}/></button>
                     </div>
 
                     <div className="flex flex-row w-full">
-                        <label htmlFor="fileInput" className="bg-secondary-bg-color  w-[20%] mr-2 rounded p-3 text-center">
+                        <label htmlFor="fileInput" className="bg-secondary-bg-color w-[20%] mr-2 rounded p-3 text-center">
                             <FontAwesomeIcon icon={faFileUpload} />
-                            <input id="fileInput" type="file" onChange={addLocalFile} />
+                            <input id="fileInput" type="file"  className="hidden " onChange={addLocalFile} />
                         </label>
-                        <button className="p-3 bg-red-500 rounded w-full" onClick={togglePopup}>Cancel</button>
+                        <button className="p-3 bg-red-500 rounded w-[80%]" onClick={togglePopup}>Cancel</button>
                     </div>
 
 
