74d30b12271b78082eca2c037ddb17d358751351
Daniel Chojnicki <djchojnicki@gmail.com>
2024-03-10 01:50:04 +0100

    finished

--- src/components/my_tasks/SortButton.tsx
+++ src/components/my_tasks/SortButton.tsx
@@ -1,23 +1,29 @@
-import React, {useState} from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faChevronUp, faChevronDown} from '@fortawesome/free-solid-svg-icons'
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronUp, faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
-const SortButton: React.FC<{   name: string; sortFunction: (isAscending: boolean) => void  }> = ({ name, sortFunction } ) => {
-    const [ isAscending, setIsAscending ] = useState(false);
+const SortButton: React.FC<{
+  name: string;
+  sortFunction?: (isAscending: boolean) => void;
+}> = ({ name, sortFunction }) => {
+  const [isAscending, setIsAscending] = useState(false);
 
-    const handleSortClick = () => {
-        sortFunction(isAscending);
-        setIsAscending((prevIsAscending) => !prevIsAscending);
-    };
-
-    return (
-        <li>
-            {name}
-            <button>
-                <FontAwesomeIcon icon={ isAscending? faChevronDown : faChevronUp} onClick={handleSortClick}/>
-            </button>
-        </li>
-    );
+  return (
+    <li className="flex flex-[1] last:flex-[0.25] items-center">
+      {name}
+      {sortFunction !== undefined && (
+        <button className="bg-none border-none text-main-font-color text-xs font-semibold p-1 rounded-md ml-2 cursor-pointer hover:bg-secondary-bg-color">
+          <FontAwesomeIcon
+            icon={isAscending ? faChevronDown : faChevronUp}
+            onClick={() => {
+              sortFunction(isAscending);
+              setIsAscending((prevIsAscending) => !prevIsAscending);
+            }}
+          />
+        </button>
+      )}
+    </li>
+  );
 };
 
 export default SortButton;
--- src/components/my_tasks/SortPanel.tsx
+++ src/components/my_tasks/SortPanel.tsx
@@ -1,49 +1,54 @@
 import React from "react";
 import TaskPreview from "../../models/TaskPreview";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faChevronUp, faChevronDown} from '@fortawesome/free-solid-svg-icons'
 import SortButton from "./SortButton";
 
-const SortPanel: React.FC<{ filteredTasks: TaskPreview[], setFilteredTasks }> = ({ filteredTasks, setFilteredTasks } ) => {
-
-    const sortByName = (isAscending) => {
-        const tasks = [...filteredTasks];
-        if(isAscending){
-            setFilteredTasks(tasks.sort((t1,t2)=>t1.name.localeCompare(t2.name)));
-        }else{
-            setFilteredTasks(tasks.sort((t1,t2)=>t2.name.localeCompare(t1.name)));
-        }
+const SortPanel: React.FC<{
+  filteredTasks: TaskPreview[];
+  setFilteredTasks: (tasks: TaskPreview[]) => void;
+}> = ({ filteredTasks, setFilteredTasks }) => {
+  const sortByName = (isAscending: boolean) => {
+    const tasks = [...filteredTasks];
+    if (isAscending) {
+      setFilteredTasks(tasks.sort((t1, t2) => t1.name.localeCompare(t2.name)));
+    } else {
+      setFilteredTasks(tasks.sort((t1, t2) => t2.name.localeCompare(t1.name)));
     }
+  };
 
-    const sortByDate = (isAscending) => {
-        const tasks = [...filteredTasks];
-        if(isAscending){
-            setFilteredTasks(tasks.sort((t1,t2)=>t1.submitDate.getTime() - t2.submitDate.getTime()));
-        }else{
-            setFilteredTasks(tasks.sort((t1,t2)=>t2.submitDate.getTime() - t1.submitDate.getTime()));
-        }
+  const sortByDate = (isAscending: boolean) => {
+    const tasks = [...filteredTasks];
+    if (isAscending) {
+      setFilteredTasks(
+        tasks.sort(
+          (t1, t2) => t1.submitDate.getTime() - t2.submitDate.getTime()
+        )
+      );
+    } else {
+      setFilteredTasks(
+        tasks.sort(
+          (t1, t2) => t2.submitDate.getTime() - t1.submitDate.getTime()
+        )
+      );
     }
+  };
 
-    const sortByCredits = (isAscending) => {
-        const tasks = [...filteredTasks];
-        if(isAscending){
-            setFilteredTasks(tasks.sort((t1,t2)=>t1.credits - t2.credits));
-        }else{
-            setFilteredTasks(tasks.sort((t1,t2)=>t2.credits - t1.credits));
-        }
+  const sortByCredits = (isAscending: boolean) => {
+    const tasks = [...filteredTasks];
+    if (isAscending) {
+      setFilteredTasks(tasks.sort((t1, t2) => t1.credits - t2.credits));
+    } else {
+      setFilteredTasks(tasks.sort((t1, t2) => t2.credits - t1.credits));
     }
+  };
 
-
-
-    return (
-        <ul className="sort-panel">
-            <SortButton name={"task name"} sortFunction={sortByName}/>
-            <SortButton name={"date solved"} sortFunction={sortByDate}/>
-            <li>status</li>
-
-            <SortButton name={"mc"} sortFunction={sortByCredits}/>
-        </ul>
-    );
+  return (
+    <ul className="w-full list-none flex justify-between pt-2 pl-2 pr-2 pb-0 font-semibold">
+      <SortButton name={"task name"} sortFunction={sortByName} />
+      <SortButton name={"date solved"} sortFunction={sortByDate} />
+      <SortButton name={"status"} />
+      <SortButton name={"mc"} sortFunction={sortByCredits} />
+    </ul>
+  );
 };
 
 export default SortPanel;
