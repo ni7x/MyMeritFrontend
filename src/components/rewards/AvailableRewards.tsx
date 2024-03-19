7f91d18fc4c5ebb366b41ad4769e860f5d735249
Daniel Chojnicki <djchojnicki@gmail.com>
2024-03-19 15:50:37 +0100

    added text-overflow to reward description

--- src/components/rewards/AvailableRewards.tsx
+++ src/components/rewards/AvailableRewards.tsx
@@ -1,18 +1,20 @@
-import {Reward as RewardDTO} from "../../models/Reward";
+import { Reward as RewardDTO } from "../../models/Reward";
 import Reward from "./Reward";
 import React from "react";
 
-const AvailableRewards: React.FC<{rewards: RewardDTO[], currentBalance:number}> = ({rewards, currentBalance}) => {
-    return (
-                <div class="flex justify-center ">
-                    <ul className="flex flex-wrap w-full gap-[2%]">
-                    {rewards.map((reward)=>{
-                        return <Reward reward={reward} currentBalance={currentBalance}/>
-                    })}
-                    </ul>
-                </div>
-
-    );
+const AvailableRewards: React.FC<{
+  rewards: RewardDTO[];
+  currentBalance: number;
+}> = ({ rewards, currentBalance }) => {
+  return (
+    <div className="flex justify-center ">
+      <ul className="flex flex-wrap w-full gap-[2%]">
+        {rewards.map((reward) => {
+          return <Reward reward={reward} currentBalance={currentBalance} />;
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default AvailableRewards;
--- src/pages/rewards/Rewards.tsx
+++ src/pages/rewards/Rewards.tsx
@@ -1,53 +1,66 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Reward from "../../models/Reward";
 import AvailableRewards from "../../components/rewards/AvailableRewards";
-import {getPurchaseHistory, getRewards} from "../../services/RewardService";
+import { getPurchaseHistory, getRewards } from "../../services/RewardService";
 import SecondWrapper from "../../components/SecondWrapper";
 import PurchaseHistory from "../../components/rewards/PurchaseHistory";
 import PurchasedReward from "../../models/PurchasedReward";
 
 const Rewards: React.FC = () => {
-    const [availableRewards, setAvailableRewards] = useState<Reward[]>([]);
-    const [purchaseHistory, setPurchaseHistory] = useState<PurchasedReward[]>([]);
+  const [availableRewards, setAvailableRewards] = useState<Reward[]>([]);
+  const [purchaseHistory, setPurchaseHistory] = useState<PurchasedReward[]>([]);
 
-    const currentBalance = 200;
+  const currentBalance = 200;
 
-    const [isHistoryTab, setIsHistoryTab] = useState(false);
+  const [isHistoryTab, setIsHistoryTab] = useState(false);
 
-    const purchaseReward = () => {
+  const purchaseReward = () => {};
 
-    }
+  useEffect(() => {
+    setAvailableRewards(getRewards);
+    setPurchaseHistory(getPurchaseHistory);
+  }, []);
 
-    useEffect(()=>{
-        setAvailableRewards(getRewards);
-        setPurchaseHistory(getPurchaseHistory)
-    }, [])
+  return (
+    <SecondWrapper>
+      <div className="flex justify-between mb-3 text-main-lighter text-sm font-medium flex-wrap">
+        <div className="h-10 mb-2 md:sm-0">
+          <button
+            onClick={() => setIsHistoryTab(true)}
+            className={`mr-2 bg-secondary-bg-color p-3 px-6 rounded ${
+              isHistoryTab ? "text-white" : ""
+            }`}
+          >
+            History
+          </button>
+          <button
+            onClick={() => setIsHistoryTab(false)}
+            className={`bg-secondary-bg-color p-3 px-6 rounded ${
+              !isHistoryTab ? "text-white" : ""
+            }`}
+          >
+            Rewards
+          </button>
+        </div>
+        <h2 className="text-center h-10 bg-secondary-bg-color flex items-center p-3 px-9 rounded">
+          {" "}
+          Balance:{" "}
+          <span class="text-merit-credits-color ml-2 font-semibold">
+            {currentBalance} MC
+          </span>
+        </h2>
+      </div>
 
-    return (
-        <SecondWrapper>
-            <div class="flex justify-between mb-3 text-main-lighter text-sm font-medium flex-wrap">
-                <div class="h-10 mb-2 md:sm-0">
-                    <button onClick={() => setIsHistoryTab(true)}
-                            className={`mr-2 bg-secondary-bg-color p-3 px-6 rounded ${isHistoryTab ? "text-white" : ""}`}>
-                        History
-                    </button>
-                    <button onClick={() => setIsHistoryTab(false)}
-                            className={`bg-secondary-bg-color p-3 px-6 rounded ${!isHistoryTab ? "text-white" : ""}`}>
-                        Rewards
-                    </button>
-                </div>
-                <h2 className="text-center h-10 bg-secondary-bg-color flex items-center p-3 px-9 rounded"> Balance:  <span class="text-merit-credits-color ml-2 font-semibold">{currentBalance} MC</span></h2>
-
-            </div>
-
-            {isHistoryTab ?
-                <PurchaseHistory history={purchaseHistory}/>
-                :
-                <AvailableRewards rewards={availableRewards}  currentBalance={currentBalance}/>
-            }
-        </SecondWrapper>
-
-    );
+      {isHistoryTab ? (
+        <PurchaseHistory history={purchaseHistory} />
+      ) : (
+        <AvailableRewards
+          rewards={availableRewards}
+          currentBalance={currentBalance}
+        />
+      )}
+    </SecondWrapper>
+  );
 };
 
 export default Rewards;
