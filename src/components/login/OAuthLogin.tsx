df912d7c93aa94cd8569227fd09608d2ee806e97
mcar <85767699+r00tens@users.noreply.github.com>
2024-03-21 00:50:05 +0100

    added hyperlinks for buttons to oauth2

--- src/components/login/OAuthLogin.tsx
+++ src/components/login/OAuthLogin.tsx
@@ -8,8 +8,14 @@ import OAuthLoginButton from './OAuthLoginButton';
 const OAuthLogin: React.FC = () => {
     return (
         <div className="flex flex-col gap-2">
-            <OAuthLoginButton icon={GoogleLogo}>Log in with Google</OAuthLoginButton>
-            <OAuthLoginButton icon={GithubLogo}>Log in with Github</OAuthLoginButton>
+            <a className={"grid"}
+               href={"http://localhost:8080/oauth2/authorize/google?redirect_uri=http://localhost:5173/oauth2/redirect"}>
+                <OAuthLoginButton icon={GoogleLogo}>Log in with Google</OAuthLoginButton>
+            </a>
+            <a className={"grid"}
+               href={"http://localhost:8080/oauth2/authorize/github?redirect_uri=http://localhost:5173/oauth2/redirect"}>
+                <OAuthLoginButton icon={GithubLogo}>Log in with Github</OAuthLoginButton>
+            </a>
         </div>
     );
 }
