f880f2fd8172765f7e81ff4338004ae8566b8e4e
mcar <85767699+r00tens@users.noreply.github.com>
2024-03-21 14:59:35 +0100

    added variables to oauth2

--- src/components/login/OAuthLogin.tsx
+++ src/components/login/OAuthLogin.tsx
@@ -9,11 +9,11 @@ const OAuthLogin: React.FC = () => {
     return (
         <div className="flex flex-col gap-2">
             <a className={"grid"}
-               href={"http://localhost:8080/oauth2/authorize/google?redirect_uri=http://localhost:5173/oauth2/redirect"}>
+               href={import.meta.env.VITE_OAUTH2_GOOGLE}>
                 <OAuthLoginButton icon={GoogleLogo}>Log in with Google</OAuthLoginButton>
             </a>
             <a className={"grid"}
-               href={"http://localhost:8080/oauth2/authorize/github?redirect_uri=http://localhost:5173/oauth2/redirect"}>
+               href={import.meta.env.VITE_OAUTH2_GITHUB}>
                 <OAuthLoginButton icon={GithubLogo}>Log in with Github</OAuthLoginButton>
             </a>
         </div>
