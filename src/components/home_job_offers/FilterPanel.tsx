c8fe2aa62a131e3c1a0c4b0c3d4679aa4c7a0eb5
ni7x <99970419+ni7x@users.noreply.github.com>
2024-05-14 14:52:14 +0200

    filter mobile made better

--- src/components/home_job_offers/FilterPanel.tsx
+++ src/components/home_job_offers/FilterPanel.tsx
@@ -44,95 +44,106 @@ const FilterPanel: React.FC<{
   const isSelected = (language: string) => {
     return queryParams.languages?.includes(language);
   };
-  console.log(queryParams.minOpensIn);
 
   return (
     <div className="h-full">
       <div
         className={
-          "max-md:absolute top-0 left-0 h-full w-full bg-secondary-bg-color px-5 py-4 mb-10 rounded lg:flex lg:flex-col justify-between " +
+          "max-md:absolute top-0 left-0 z-[1000] w-[100%] bg-secondary-bg-color px-5 py-4 mb-10 rounded lg:flex lg:flex-col justify-between " +
           (isPopupOpen ? "popup-open" : "hidden")
         }
       >
-        <div className="h-[90%] flex flex-col">
-          <button
-            onClick={togglePopup}
-            className={
-              "text-white bg-red-400 w-10 h-10 font-bold text-sm rounded-[50%] self-end mb-5 " +
-              (isPopupOpen ? "" : "hidden")
-            }
-          >
-            <FontAwesomeIcon icon={faX} />
-          </button>
-          <div className="flex flex-col items-center lg:items-start">
-            <label className="pb-5 lg:pb-2 text-sm font-medium">
-              Languages
-            </label>
-            <div className="flex flex-wrap text-sm font-medium">
-              {languages.map((language) => {
-                return (
-                  <button
-                    onClick={() => toggleLanguage(language)}
-                    key={language}
-                    className={
-                      "px-5 py-2 rounded mr-2 mb-3 " +
-                      (isSelected(language)
-                        ? " border-2 border-emerald-400 text-emerald-300"
-                        : " bg-main-lighter-2 border-2 border-main-lighter-2 ")
-                    }
-                  >
-                    {language}
-                  </button>
-                );
-              })}
+        <div className="lg:mt-0 h-[100%] flex flex-col">
+          <div className={"flex justify-between border-b-[1px] mb-5 border-main-border "  +(isPopupOpen ? "" : "hidden")}>
+            <p className="font-medium text-sm text-task-lighter">FILTERS
+            </p>
+            <button
+                onClick={togglePopup}
+                className={
+                    "text-white text-sm  mb-3 self-end hover:underline " +
+                    (isPopupOpen ? "" : "hidden")
+                }
+            >
+              CLOSE
+            </button>
+          </div>
+          <div className="flex flex-col h-full w-full gap-3">
+            <div className="flex flex-col  lg:items-start">
+              <label className="pb-2.5 lg:pb-2 text-sm font-medium">
+                Languages
+              </label>
+              <div className="flex flex-wrap text-sm font-medium">
+                {languages.map((language) => {
+                  return (
+                      <button
+                          onClick={() => toggleLanguage(language)}
+                          key={language}
+                          className={
+                              "px-5 py-2 rounded mr-2 mb-3 " +
+                              (isSelected(language)
+                                  ? " border-2 border-emerald-400 text-emerald-300"
+                                  : " bg-main-lighter-2 border-2 border-main-lighter-2 ")
+                          }
+                      >
+                        {language}
+                      </button>
+                  );
+                })}
+              </div>
+            </div>
+            <div className="flex flex-col pt-2 text-sm pb-2 lg:justify-center  lg:items-stretch">
+              <RangeInput
+                  label="Merit Credits"
+                  min={0}
+                  max={1000}
+                  minValue={queryParams.minCredits}
+                  maxValue={queryParams.maxCredits}
+                  onInputChange={(e) =>
+                      handleFilterChange("minCredits", e.minValue)
+                  }
+              />
+            </div>
+            <div className="flex flex-col pt-2 pb-2  lg:items-stretch">
+              <label className="pb-1.5 lg:pb-2 text-sm font-medium">
+                Opens From
+              </label>
+              <input
+                  type="date"
+                  className="outline-none mt-1 p-2 px-3 w-[100%] rounded bg-main-lighter-2"
+                  onChange={(e) => handleFilterChange("minOpensIn", e.target.value)}
+              />
+            </div>
+
+            <div className="flex flex-col pt-2 pb-2  lg:items-stretch">
+              <label className="pb-1.5 lg:pb-2 text-sm font-medium">Opens To</label>
+              <input
+                  type="date"
+                  className="outline-none mt-1 p-2 px-3 w-[100%] rounded bg-main-lighter-2"
+                  onChange={(e) => handleFilterChange("maxOpensIn", e.target.value)}
+              />
+            </div>
+            <div className="flex flex-col pt-2 text-sm pb-2 lg:justify-center lg:items-stretch">
+              <RangeInput
+                  label="Salary"
+                  min={0}
+                  max={40000}
+                  minValue={queryParams.minSalary}
+                  maxValue={queryParams.maxSalary}
+                  currency="$"
+                  onInputChange={(e) => handleFilterChange("minSalary", e.minValue)}
+              />
             </div>
           </div>
-          <div className="flex flex-col pt-2 text-sm pb-2 lg:justify-center items-center lg:items-stretch">
-            <RangeInput
-              label="Merit Credits"
-              min={0}
-              max={1000}
-              minValue={queryParams.minCredits}
-              maxValue={queryParams.maxCredits}
-              onInputChange={(e) =>
-                handleFilterChange("minCredits", e.minValue)
-              }
-            />
-          </div>
-          <div className="flex flex-col pt-2 pb-2 items-center lg:items-stretch">
-            <label className="pb-5 lg:pb-2 text-sm font-medium">
-              Opens From
-            </label>
-            <input
-              type="date"
-              className="outline-none mt-1 p-2 px-3 w-[100%] rounded bg-main-lighter-2"
-              onChange={(e) => handleFilterChange("minOpensIn", e.target.value)}
-            />
-          </div>
-
-          <div className="flex flex-col pt-2 pb-2 items-center lg:items-stretch">
-            <label className="pb-5 lg:pb-2 text-sm font-medium">Opens To</label>
-            <input
-              type="date"
-              className="outline-none mt-1 p-2 px-3 w-[100%] rounded bg-main-lighter-2"
-              onChange={(e) => handleFilterChange("maxOpensIn", e.target.value)}
-            />
-          </div>
-          <div className="flex flex-col pt-2 text-sm pb-2 lg:justify-center items-center lg:items-stretch">
-            <RangeInput
-              label="Salary"
-              min={0}
-              max={40000}
-              minValue={queryParams.minSalary}
-              maxValue={queryParams.maxSalary}
-              currency="$"
-              onInputChange={(e) => handleFilterChange("minSalary", e.minValue)}
-            />
-          </div>
+          <button
+              className={"w-[100%] m-auto bg-emerald-400 text-sm mt-5 font-semibold rounded p-3 " + (isPopupOpen ? "" : "hidden")}
+              onClick={togglePopup}
+          >
+            FILTER
+          </button>
         </div>
       </div>
       <button
-        className="lg:hidden text-center appearance-none text-sm font-medium outline-none py-3 px-5 mb-3 rounded bg-secondary-bg-color "
+        className="lg:hidden text-center appearance-none text-sm font-medium outline-none py-3 px-5 rounded bg-secondary-bg-color "
         onClick={togglePopup}
       >
         {isPopupOpen ? "Hide filters" : "Show filters"}
