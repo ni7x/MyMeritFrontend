cce20cf9b81235d21ef2ccc40710cbced71fd07f
ni7x <99970419+ni7x@users.noreply.github.com>
2024-05-22 17:54:44 +0200

    refactir

--- src/components/home_job_offers/JobOfferDetailsList.tsx
+++ src/components/home_job_offers/JobOfferDetailsList.tsx
@@ -5,102 +5,92 @@ import JobOfferListedDTO from "../../models/dtos/JobOfferListedDTO";
 import TaskStatus from "../../models/TaskStatus";
 import TechnologyTags from "./TechnologyTags";
 
+const languageColors = {
+    JAVA: "#ec9b2a",
+    PYTHON: "#40a5f8",
+    JAVASCRIPT: "#f1e05a",
+    CPP: "#f34b7d",
+    GO: "#00ADD8",
+    KOTLIN: "#A97BFF",
+    TYPESCRIPT: "#46d595",
+    PHP: "#c72eb3",
+};
+
+const CompanyDetails: React.FC<{ jobOffer: JobOfferListedDTO }> = ({ jobOffer }) => (
+    <p className="flex items-center w-[20rem] xs:w-auto">
+        <img
+            src={jobOffer.company.imageBase64}
+            className="block xs:hidden w-[1.5rem] h-[1.5rem] rounded mr-2 object-cover img-anchor"
+            alt={jobOffer.company.username}
+        />
+        {jobOffer.company.username}
+    </p>
+);
+
+const WorkLocations: React.FC<{ jobOffer: JobOfferListedDTO }> = ({ jobOffer }) => (
+    <div className="flex gap-2 text-task-lighter items-center">
+        <FontAwesomeIcon icon={faLocationDot} />
+        <ul className="flex">
+            {jobOffer.workLocations.map((location, index) => (
+                <li key={location}>
+                    {location.toUpperCase()}
+                    {index !== jobOffer.workLocations.length - 1 && (
+                        <span className="mx-1">/</span>
+                    )}
+                </li>
+            ))}
+        </ul>
+    </div>
+);
+
+const Experience: React.FC<{ jobOffer: JobOfferListedDTO }> = ({ jobOffer }) => (
+    <div className="flex gap-2 text-task-lighter items-center">
+        <FontAwesomeIcon icon={faGraduationCap} />
+        {jobOffer.experience}
+    </div>
+);
+
 const JobOfferDetailsList: React.FC<{ jobOffer: JobOfferListedDTO, isFullView: boolean }> = ({ jobOffer, isFullView }) => {
-    const languageColors: { [key: string]: string } = {
-        JAVA: "#ec9b2a",
-        PYTHON: "#40a5f8",
-        JAVASCRIPT: "#f1e05a",
-        CPP: "#f34b7d",
-        GO: "#00ADD8",
-        KOTLIN: "#A97BFF",
-        TYPESCRIPT: "#46d595",
-        PHP: "#c72eb3",
-    };
     return (
         <div>
             <div className="flex items-center px-4 pb-4 mt-1">
-            <img
-                src={jobOffer.company.imageBase64}
-                className="hidden xs:block w-[4.5rem] h-[4.5rem] rounded object-cover img-anchor"
-                alt={jobOffer.company.username}
-            />
-            <div className={"flex h-full xs:pl-4 w-full gap-3 xs:gap-1 flex-col"}>
-                <h3 className="text-xl font-semibold w-full hover:underline">
-                    <a href={"job/" + jobOffer.id + (jobOffer.status === TaskStatus.NOT_YET_OPEN ? "" : "/solution")}>
-                        {jobOffer.jobTitle}
-                    </a>
-                </h3>
-                {!isFullView &&
-                    <div className="pt-1">
-                        <TechnologyTags technologies={jobOffer.technologies} languageColors={languageColors} />
-                    </div>
-                }
-                {isFullView &&
-                    <div className="flex w-full sm:gap-6 xs:gap-4 gap-3 text-sm font-medium xs:justify-between flex-wrap">
-                        <div className="flex flex-wrap sm:gap-x-7 gap-x-4 gap-y-4">
-                            <p className="flex items-center w-[20rem] xs:w-auto">
-                                <img
-                                    src={jobOffer.company.imageBase64}
-                                    className="block xs:hidden w-[1.5rem] h-[1.5rem] rounded mr-2 object-cover img-anchor"
-                                    alt={jobOffer.company.username}
-                                />
-                                {jobOffer.company.username}
-                            </p>
-                            <div className="flex gap-2 text-task-lighter items-center">
-                                <FontAwesomeIcon icon={faLocationDot} />
-                                <ul className="flex">
-                                    {jobOffer.workLocations.map((location, index) => (
-                                        <li key={location}>
-                                            {location.toUpperCase()}
-                                            {index !== jobOffer.workLocations.length - 1 && (
-                                                <span className="mx-1">/</span>
-                                            )}
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                            <div className="flex gap-2 text-task-lighter items-center">
-                                <FontAwesomeIcon icon={faGraduationCap} />
-                                {jobOffer.experience}
-                            </div>
+                <img
+                    src={jobOffer.company.imageBase64}
+                    className="hidden xs:block w-[4.5rem] h-[4.5rem] rounded object-cover img-anchor"
+                    alt={jobOffer.company.username}
+                />
+                <div className="flex h-full xs:pl-4 w-full gap-3 xs:gap-1 flex-col">
+                    <h3 className="text-xl font-semibold w-full hover:underline">
+                        <a href={`job/${jobOffer.id}${jobOffer.status === TaskStatus.NOT_YET_OPEN ? "" : "/solution"}`}>
+                            {jobOffer.jobTitle}
+                        </a>
+                    </h3>
+                    {!isFullView && (
+                        <div className="pt-1">
+                            <TechnologyTags technologies={jobOffer.technologies} languageColors={languageColors} />
                         </div>
-                        <TechnologyTags technologies={jobOffer.technologies} languageColors={languageColors} />
-
-                    </div>}
+                    )}
+                    {isFullView && (
+                        <div className="flex w-full sm:gap-6 xs:gap-4 gap-3 text-sm font-medium xs:justify-between flex-wrap">
+                            <div className="flex flex-wrap sm:gap-x-7 gap-x-4 gap-y-4">
+                                <CompanyDetails jobOffer={jobOffer} />
+                                <WorkLocations jobOffer={jobOffer} />
+                                <Experience jobOffer={jobOffer} />
+                            </div>
+                            <TechnologyTags technologies={jobOffer.technologies} languageColors={languageColors} />
+                        </div>
+                    )}
+                </div>
             </div>
-        </div>
-            {!isFullView && <>
+            {!isFullView && (
                 <div className="flex w-full sm:gap-6 xs:gap-4 gap-3 text-sm font-medium xs:justify-between flex-wrap pb-4 px-4">
                     <div className="flex flex-wrap sm:gap-x-7 gap-x-4 gap-y-4">
-                        <p className="flex items-center w-[20rem] xs:w-auto">
-                            <img
-                                src={jobOffer.company.imageBase64}
-                                className="block xs:hidden w-[1.5rem] h-[1.5rem] rounded mr-2 object-cover img-anchor"
-                                alt={jobOffer.company.username}
-                            />
-                            {jobOffer.company.username}
-                        </p>
-                        <div className="flex gap-2 text-task-lighter items-center">
-                            <FontAwesomeIcon icon={faLocationDot} />
-                            <ul className="flex">
-                                {jobOffer.workLocations.map((location, index) => (
-                                    <li key={location}>
-                                        {location.toUpperCase()}
-                                        {index !== jobOffer.workLocations.length - 1 && (
-                                            <span className="mx-1">/</span>
-                                        )}
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                        <div className="flex gap-2 text-task-lighter items-center">
-                            <FontAwesomeIcon icon={faGraduationCap} />
-                            {jobOffer.experience}
-                        </div>
+                        <CompanyDetails jobOffer={jobOffer} />
+                        <WorkLocations jobOffer={jobOffer} />
+                        <Experience jobOffer={jobOffer} />
                     </div>
-
                 </div>
-            </>}
+            )}
         </div>
     );
 };
