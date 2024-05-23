5f8e3cb4b166f5a2196c327e2494583bec7d34b9
mcar <85767699+r00tens@users.noreply.github.com>
2024-05-23 23:03:48 +0200

    errors related to NoItemsFound removed

--- src/components/NoItemsFound.tsx
+++ src/components/NoItemsFound.tsx
@@ -1,20 +1,40 @@
-import React from 'react';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faBan} from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBan } from "@fortawesome/free-solid-svg-icons";
 
-const NoItemsFound = ({ itemName = "items", customMessage, customIcon, customStyles = {} }) => {
-    return (
-        <div className="flex flex-col items-center justify-center h-full" style={customStyles}>
-            {customIcon ? (
-                customIcon
-            ) : (
-                <>
-                    <FontAwesomeIcon  className="h-14 w-14 text-task-lighter" icon={faBan}/>
-                </>
-            )}
-            <p className="mt-4 text-task-lighter text-lg font-medium">{customMessage ? customMessage : `No ${itemName} found.`}</p>
-        </div>
-    );
+interface NoItemsFoundProps {
+  itemName?: string;
+  customMessage?: React.ReactNode;
+  customIcon?: React.ReactElement;
+  customStyles?: React.CSSProperties;
+}
+
+const NoItemsFound: React.FC<NoItemsFoundProps> = ({
+  itemName = "items",
+  customMessage,
+  customIcon,
+  customStyles = {},
+}) => {
+  return (
+    <div
+      className="flex flex-col items-center justify-center h-full"
+      style={customStyles}
+    >
+      {customIcon ? (
+        customIcon
+      ) : (
+        <>
+          <FontAwesomeIcon
+            className="h-14 w-14 text-task-lighter"
+            icon={faBan}
+          />
+        </>
+      )}
+      <p className="mt-4 text-task-lighter text-lg font-medium">
+        {customMessage ? customMessage : `No ${itemName} found.`}
+      </p>
+    </div>
+  );
 };
 
 export default NoItemsFound;
--- src/components/home_job_offers/JobOfferList.tsx
+++ src/components/home_job_offers/JobOfferList.tsx
@@ -3,14 +3,25 @@ import JobOffer from "./JobOffer";
 import JobOfferListedDTO from "../../models/dtos/JobOfferListedDTO";
 import NoItemsFound from "../NoItemsFound";
 
-const JobOfferList: React.FC<{ jobOffers: JobOfferListedDTO[], isFullView: boolean }> = ({ jobOffers, isFullView }) => {
+const JobOfferList: React.FC<{
+  jobOffers: JobOfferListedDTO[];
+  isFullView: boolean;
+}> = ({ jobOffers, isFullView }) => {
   return (
-    <div
-      className={`w-full flex flex-row flex-wrap align-center gap-4`}
-    >
-      {!jobOffers || jobOffers.length === 0
-        ? <div className="h-[32rem] w-full"><NoItemsFound itemName="tasks"/></div>
-        : jobOffers.map((jobOffer) => <JobOffer isFullView={isFullView} key={jobOffer.id} jobOffer={jobOffer} />)}
+    <div className={`w-full flex flex-row flex-wrap align-center gap-4`}>
+      {!jobOffers || jobOffers.length === 0 ? (
+        <div className="h-[32rem] w-full">
+          <NoItemsFound itemName="tasks" />
+        </div>
+      ) : (
+        jobOffers.map((jobOffer) => (
+          <JobOffer
+            isFullView={isFullView}
+            key={jobOffer.id}
+            jobOffer={jobOffer}
+          />
+        ))
+      )}
     </div>
   );
 };
